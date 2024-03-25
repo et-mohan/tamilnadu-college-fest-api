b7b2159414c17a14b8cb5cb5fe01507b3362f27a
Your Name <your.email@example.com>
2024-03-25 21:19:39 +0530

    optimized

--- index.js
+++ index.js
@@ -6,43 +6,47 @@ const cheerio = require('cheerio');
 
 const app = express();
 
+// Reusing axios instance
+const axiosInstance = axios.create();
+
 app.get('/', function (req, res) {
     res.json("Hello, welcome to my website.");
 });
 
 app.get('/fests', async function (req, res) {
     try {
-        const response = await axios.get('https://www.knowafest.com/explore/state/Tamil-Nadu');
+        const response = await axiosInstance.get('https://www.knowafest.com/explore/state/Tamil-Nadu');
         const html = response.data;
         const $ = cheerio.load(html);
-        const eventDataPromises = $('table:contains(tr) tr:not(:first-child)').map(async (index, element) => {
+        const eventData = [];
+
+        // Iterate synchronously using for...of loop
+        for (const element of $('table:contains(tr) tr:not(:first-child)')) {
             const tds = $(element).find('td');
             const eventUrl = $(element).attr('onclick');
             const modifiedEventurl = eventUrl.replace("window.open('..", '').replace("' );", '');
-            const mainUrl='https://www.knowafest.com/explore'+modifiedEventurl;
-            
-            const response = await axios.get(mainUrl)
-            .then(function(response){
+            const mainUrl = 'https://www.knowafest.com/explore' + modifiedEventurl;
+
+            try {
+                const response = await axiosInstance.get(mainUrl);
                 const html1 = response.data;
                 const $1 = cheerio.load(html1);
                 const registerLink = $1('a:contains("Register now")').attr('href');
-                return registerLink;
-                // console.log(registerLink);
-            });
-            
-            // console.log(html1);
-            
-            return {
-                startDate: $(tds[0]).text().trim(),
-                festName: $(tds[1]).text().trim().replace(/ View More$/, ''),
-                festType: $(tds[2]).text().trim(),
-                collegeName: $(tds[3]).text().trim(),
-                city: $(tds[4]).text().trim(),
-                registerLink: response,
-            };
-        }).get();
-        
-        const eventData = await Promise.all(eventDataPromises);
+
+                eventData.push({
+                    startDate: $(tds[0]).text().trim(),
+                    festName: $(tds[1]).text().trim().replace(/ View More$/, ''),
+                    festType: $(tds[2]).text().trim(),
+                    collegeName: $(tds[3]).text().trim(),
+                    city: $(tds[4]).text().trim(),
+                    registerLink: registerLink
+                });
+            } catch (err) {
+                console.error(err);
+                // Don't send response here, continue to the next iteration
+            }
+        }
+
         res.json({
             status: 'success',
             data: eventData

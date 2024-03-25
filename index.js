bd8b0c361163dcd10df106a788a8b9d3da546b46
Your Name <your.email@example.com>
2024-03-25 19:41:46 +0530

    updated registration link

--- index.js
+++ index.js
@@ -10,37 +10,50 @@ app.get('/', function (req, res) {
     res.json("Hello, welcome to my website.");
 });
 
-app.get('/news', function (req, res) {
-    axios.get('https://www.knowafest.com/explore/state/Tamil-Nadu')
-        .then(function (response) {
-            const html = response.data;
-            const $ = cheerio.load(html);
-
-            // Extracting table data
-            const eventData = $('table:contains(tr) tr:not(:first-child)').map((index, element) => {
-                const tds = $(element).find('td');
-                return {
-                    startDate: $(tds[0]).text().trim(),
-                    festName: $(tds[1]).text().trim(),
-                    FestType: $(tds[2]).text().trim(),
-                    CollegeName: $(tds[3]).text().trim(),
-                    city: $(tds[4]).text().trim()
-                };
-            }).get();
-
-            // Sending the extracted data as response
-            res.json({
-                status: 'success',
-                data: eventData
+app.get('/fests', async function (req, res) {
+    try {
+        const response = await axios.get('https://www.knowafest.com/explore/state/Tamil-Nadu');
+        const html = response.data;
+        const $ = cheerio.load(html);
+        const eventDataPromises = $('table:contains(tr) tr:not(:first-child)').map(async (index, element) => {
+            const tds = $(element).find('td');
+            const eventUrl = $(element).attr('onclick');
+            const modifiedEventurl = eventUrl.replace("window.open('..", '').replace("' );", '');
+            const mainUrl='https://www.knowafest.com/explore'+modifiedEventurl;
+            
+            const response = await axios.get(mainUrl)
+            .then(function(response){
+                const html1 = response.data;
+                const $1 = cheerio.load(html1);
+                const registerLink = $1('a:contains("Register now")').attr('href');
+                return registerLink;
+                // console.log(registerLink);
             });
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).json({
-                status: 'error',
-                message: 'Internal server error'
-            });
+            
+            // console.log(html1);
+            
+            return {
+                startDate: $(tds[0]).text().trim(),
+                festName: $(tds[1]).text().trim().replace(/ View More$/, ''),
+                festType: $(tds[2]).text().trim(),
+                collegeName: $(tds[3]).text().trim(),
+                city: $(tds[4]).text().trim(),
+                registerLink: response,
+            };
+        }).get();
+        
+        const eventData = await Promise.all(eventDataPromises);
+        res.json({
+            status: 'success',
+            data: eventData
         });
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({
+            status: 'error',
+            message: 'Internal server error'
+        });
+    }
 });
 
 app.listen(PORT, () => {

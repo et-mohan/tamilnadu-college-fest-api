9723694a8e0e0c9112db6440022c4e100aead659
Your Name <your.email@example.com>
2024-04-15 18:34:02 +0530

    removed view more

--- index.js
+++ index.js
@@ -23,13 +23,12 @@ app.get('/fests', function (req, res) {
         .then(function (response) {
             const html = response.data;
             const $ = cheerio.load(html);
-
             // Extracting table data
             const eventData = $('table:contains(tr) tr:not(:first-child)').map((index, element) => {
                 const tds = $(element).find('td');
                 return {
                     startDate: $(tds[0]).text().trim(),
-                    festName: $(tds[1]).text().trim(),
+                    festName: $(tds[1]).text().trim().replace(" View More", ""),
                     FestType: $(tds[2]).text().trim(),
                     CollegeName: $(tds[3]).text().trim(),
                     city: $(tds[4]).text().trim()
